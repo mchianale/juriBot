cb7801819bcc3a55cc7e7414dd3a048adf2b3825
Chianale Matteo <matteo.chianale75@gmail.com>
2025-01-23 22:00:01 +0100

    add exemple

--- streamLitFront/app.py
+++ streamLitFront/app.py
@@ -12,8 +12,13 @@ if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "Bonjour, je suis Juribot, un agent conversationnel spécialisé dans le domaine du droit relatifs au Code de l'Éducation, propulsé par l'intelligence artificielle de Mistral. Saisissez votre question juridique ci-dessous, et voyons ensemble si je peux vous apporter une réponse pertinente."}]
 
 for msg in st.session_state.messages:
-    if msg["role"] in ['user', 'assistant']:
+    if msg["role"] in ['user', 'assistant'] and msg["content"] != "":
         st.chat_message(msg["role"]).write(msg["content"])
+    if msg["role"] == "tool":
+        documents =  ast.literal_eval(msg["content"])
+        for document in documents:
+            content = ':'.join(document['document'].split(':')[-1:])
+            st.chat_message('⚖️').write(f"**{document['metadata']['id']}**:\n{content}")
 
 if prompt := st.chat_input():
     # write the prompt
@@ -26,9 +31,7 @@ if prompt := st.chat_input():
     if response["status_code"] == 200:
         response = response["response"]
         st.session_state.messages += response
-        # write assistant
-        #st.chat_message("tool").write(st.session_state.messages[-2]['content'])
-        #st.error(response)
+    
         for message in response:
             if message["role"] == "tool":
                 documents =  ast.literal_eval(message["content"])
